3a93141f7a979fbf2ce6dc8f7e41d4d71e2d1f2d
constantino-garcia <constantinoantoniogm@gmail.com>
2020-09-29 11:09:54 +0200

    Add train function

--- utils/train.py
+++ utils/train.py
@@ -1,62 +1,185 @@
 import os
+
+from vaele_config import tf_floatx
 import numpy as np
-from utils.tf_utils import floatx
+import tensorflow as tf
+import tensorflow_probability as tfp
+tfd = tfp.distributions
+
+from SdeModel import SdeModel
+from VAE import VAE
+from utils.math import build_delay_space
 
 
-# Adapted from https://r2rt.com/recurrent-neural-networks-in-tensorflow-i.html
-def create_batch_gen(raw_data, batch_size, len_tbptt):
-    """
-    From the embedded time series raw_data, we create a Batch for training shaped as
-    [batch_size, len_tbptt, embedding_dimension]
-    If our lag_embedded_y is a bidimensional series (1, 2), (3, 4), (5, 6), ..., (19, 20)
-    len_tbptt = 3 and batch_size = 2, we rearrange raw_data as data:
-    (1, 2),   (3, 4),   (5, 6),  (7, 8), (9, 10)
-    (11, 12), (13, 14), (15,16), ...,     (19, 20)
-    etc.
-    For a single train step, we return data[:, i * len_tbptt : (i + 1) * len_tbptt]
-    A whole epoch exhausts the columns of data
-
-    :param raw_data: A m-dimensional time series of length T arranged as a matrix (T, m).
-    :param batch_size: Number of batches in the training step.
-    :param len_tbptt: Number of time steps before truncating the gradient (tbptt: Truncated Backprop through time).
-    :return: An iterator that returns a single batch for the training procedure.
-    """
-    data_length, dimension = raw_data.shape
-    # partition raw data into batches and stack them vertically in a data matrix
-    batch_partition_length = data_length // batch_size
-    if batch_partition_length < len_tbptt:
-        raise ValueError('batch_partition_len < len_tbptt')
-    data = np.zeros([batch_size, batch_partition_length, dimension], dtype=floatx())
-    for i in range(batch_size):
-        data[i] = raw_data[batch_partition_length * i:batch_partition_length * (i + 1), :]
-    # further divide batch partitions into num_steps for truncated backpropagation
-    epoch_size = batch_partition_length // len_tbptt
-    for i in range(epoch_size):
-        # There is no need to check bounds since epoch_size was calculated with //
-        yield data[:, i * len_tbptt:(i + 1) * len_tbptt, :]
+def draw_fast_samples(vae, initial_state, x_chunk):
+    rnn_state, mean0, scale0 = vae._handle_x0_state(initial_state, x_chunk)
+    mean, scales, states = vae.encoder(x_chunk, training=True, initial_state=rnn_state)
+    distro = tfd.MultivariateNormalDiag(mean, scales)
+    samples = distro.sample(vae.mpa.nb_samples)
+    covs = tfp.stats.covariance(tf.reshape(samples, (*samples.shape[:2], -1)), sample_axis=0)
+    # TODO
+    covs = covs + 1e-8 * tf.eye(covs.shape[1], batch_shape=[covs.shape[0]], dtype=tf_floatx())
+    entropies = 0.5 * covs.shape[1] * (
+            1 + tf.math.log(2 * tf.constant(np.pi, dtype=tf_floatx()))) + 0.5 * tf.linalg.logdet(covs)
+    return samples, entropies, (mean, scales), (mean0, scale0), states
 
 
-def create_epoch_gen(raw_data, nb_epochs, batch_size, len_tbptt):
-    for i in range(nb_epochs):
-        yield create_batch_gen(raw_data, batch_size, len_tbptt)
+def add_bursts(y, distros=None, min_noise=0.02, noise_std=1):
+    events = np.zeros_like(y)
+    for i in range(events.shape[0]):
+        start = np.random.choice(np.arange(y.shape[-2]))
+        length = int(np.random.default_rng().exponential(20))
+        if length > 0:
+            events[i, start:(start + length), :] = 1
+    noise = min_noise + np.random.normal(0, noise_std, size=events.shape) * events
+    return y + tf.convert_to_tensor(noise)
 
 
-# TODO: we should use to generators as in https://r2rt.com/recurrent-neural-networks-in-tensorflow-i.html
-def get_epoch_size(raw_data, batch_size, len_tbptt):
-    data_length, dimension = raw_data.shape
-    batch_partition_length = data_length // batch_size
-    if batch_partition_length < len_tbptt:
-        raise ValueError('batch_partition_len < len_tbptt')
-    epoch_size = batch_partition_length // len_tbptt
-    return epoch_size
+def tbptt_chunks_generator(data_, len_tbptt, time_lag, kernel_size, dilation_rate,
+                           noise_std=0, do_bursts=True):
+    # TODO
+    # rossler 3, 15
+    # lorenz, 5, time_lag
+    target, data = build_delay_space(data_, 5, time_lag)
+    prediction_lag = 0
+    len_tbptt = min(data.shape[1], len_tbptt)
+    do_bursts = False
+    # Length that can be used to generate both the lagged version and the target signal from
+    # the original data
+    nb_drop = (kernel_size - 1) * dilation_rate
+    nb_chunks = int(np.floor((data.shape[1] - max(prediction_lag + nb_drop, nb_drop)) / len_tbptt))
+    if nb_chunks < 1:
+        raise RuntimeError('Cannot generate chunks in tbptt')
+    for i in range(nb_chunks):
+        # Add extra samples to handle that the convolution removes the nb_drop lattest ones
+        inputs = data[:, (i * len_tbptt):((i + 1) * len_tbptt + nb_drop), :]
+        # FIXME
+        # output = tf.concat([
+        #     data[:, (i * len_tbptt + nb_drop):((i + 1) * len_tbptt + nb_drop), :],
+        #     data[:, (i * len_tbptt + prediction_lag + nb_drop):((i + 1) * len_tbptt + prediction_lag + nb_drop), :],
+        # ], axis=-1)
+        # output = output[:, nb_drop:(-nb_drop), :]
+        output = target[:, (i * len_tbptt):((i + 1) * len_tbptt + nb_drop), :]
+        # TODO: noise
+        if do_bursts:
+            inputs = add_bursts(inputs)
+        elif noise_std > 0:
+            inputs = inputs + tf.random.normal(inputs.shape, stddev=noise_std, dtype=tf_floatx())
+        yield inputs, output
 
 
-def prepare_training_dirs(tensorboard_directory, save_path, do_run):
-    if not do_run:
-        if os.path.exists(tensorboard_directory):
-            import shutil
-            shutil.rmtree(tensorboard_directory)
-        if os.path.exists(save_path):
-            import shutil
-            shutil.rmtree(save_path)
-        os.makedirs(save_path)
+@tf.function
+def optimize_nnets_and_hpars(y_input, y_target, optimizer, gm: VAE, initial_state=None,
+                             effective_nb_timesteps=None,
+                             kl_weight=tf.convert_to_tensor(1.0, dtype=tf_floatx())):
+    vars = (
+            gm.encoder.trainable_variables +
+            list(gm.decoder.trainable_variables) +
+            list(gm.sde_model.hyperpars)
+    )
+    with tf.GradientTape(persistent=False, watch_accessed_variables=False) as tape:
+        tape.watch(vars)
+        (samples, entropies, sampling_dist), encoded_dist, decoded_dist, final_state = gm._encode_and_decode(
+            y_input, training=True, initial_state=initial_state
+        )
+        bloss = gm._breaked_loss(
+            y_input, y_target, samples, entropies, encoded_dist, decoded_dist, initial_state,
+            effective_nb_timesteps, kl_weight
+        )
+        loss = tf.reduce_sum(bloss)
+    grads = tape.gradient(loss, vars)
+    optimizer.apply_gradients(zip(grads, vars))
+    return loss, final_state
+
+
+# @tf.function
+# def optimize_sde_hpars(y_input, y_target, hpars_optimizer, gm, initial_state=None,
+#                        effective_nb_timesteps=None,
+#                        kl_weight=tf.convert_to_tensor(1.0, dtype=tf_floatx()), clip_value=100.):
+#     hpars = list(gm.sde_model.hyperpars)
+#     (samples, entropies, _), encoded_dist, decoded_dist, final_state, _ = gm._encode_and_decode(
+#         y_input, training=True, initial_state=initial_state
+#     )
+#     with tf.GradientTape() as tape:
+#         tape.watch(hpars)
+#         breaked_loss = gm._breaked_loss(
+#             y_target, samples, entropies, encoded_dist, decoded_dist, initial_state, effective_nb_timesteps, kl_weight
+#         )
+#         loss = tf.reduce_sum(breaked_loss)
+#     hgrads = tape.gradient(loss, hpars)
+#     hpars_optimizer.apply_gradients(zip(hgrads, hpars))
+#     return loss, breaked_loss, final_state
+
+
+
+@tf.function
+def optimize_sde_standard_grad(y_input, y_target, gm:VAE, optimizer, initial_state=None,
+                              effective_nb_timesteps=None,
+                              kl_weight=tf.convert_to_tensor(1.0, dtype=tf_floatx()),
+                              clip_value=100.):
+   vvars = gm.sde_model.variational_variables
+   (samples, entropies, _), encoded_dist, decoded_dist, final_state = gm._encode_and_decode(
+       y_input, training=False, initial_state=initial_state
+   )
+   with tf.GradientTape(persistent=False, watch_accessed_variables=False) as tape:
+       tape.watch(vvars)
+       breaked_loss = gm._breaked_loss(
+           y_input, y_target, samples, entropies, encoded_dist, decoded_dist,
+           initial_state, effective_nb_timesteps, kl_weight
+       )
+       loss = tf.reduce_sum(breaked_loss)
+       # loss = gm.loss(y_input, y_target, training=True)
+   vgrads = tape.gradient(loss, vvars)
+   optimizer.apply_gradients(zip(vgrads, vvars))
+   return loss, breaked_loss, final_state
+#
+
+
+@tf.function
+def optimize_sde_with_nat_grad(y_input, y_target, gm,
+                               gamma=1.0, initial_state=None, effective_nb_timesteps=None,
+                               kl_weight=tf.convert_to_tensor(1.0, dtype=tf_floatx()),
+                               clip_value=None):
+    sde_nat_grads, breaked_loss, loss, final_state = gm.nat_grads(
+        y_input, y_target, training=False, initial_state=initial_state,
+        effective_nb_timesteps=effective_nb_timesteps,
+        kl_weight=kl_weight
+    )
+    if clip_value:
+        sde_nat_grads = [tf.clip_by_value(nat_grad, -clip_value, clip_value) for nat_grad in sde_nat_grads]
+
+    thetas = SdeModel.standard_to_natural_params(
+        [gm.sde_model.diffusion._alphas.value(), gm.sde_model.diffusion._betas.value(),
+         gm.sde_model.drift_svgp.q_mu.value(), gm.sde_model.drift_svgp.q_sqrt.value()]
+    )
+    new_xis = SdeModel.natural_to_standard_params(
+        (thetas[0] - gamma * sde_nat_grads[0],
+         thetas[1] - gamma * sde_nat_grads[1],
+         thetas[2] - gamma * sde_nat_grads[2],
+         thetas[3] - gamma * sde_nat_grads[3])
+    )
+    gm.sde_model.diffusion._alphas.assign(new_xis[0])
+    gm.sde_model.diffusion._betas.assign(new_xis[1])
+    gm.sde_model.drift_svgp.q_mu.assign(new_xis[2])
+    gm.sde_model.drift_svgp.q_sqrt.assign(new_xis[3])
+
+    return loss, breaked_loss, final_state
+
+
+def get_breaked_loss(y_input, y_target, gm,
+                     gamma=1.0, initial_state=None, effective_nb_timesteps=None,
+                     kl_weight=tf.convert_to_tensor(1.0, dtype=tf_floatx()),
+                     clip_value=None):
+    sde_nat_grads, breaked_loss, loss, final_state = gm.nat_grads(
+        y_input, y_target, training=False, initial_state=initial_state,
+        effective_nb_timesteps=effective_nb_timesteps,
+        kl_weight=kl_weight
+    )
+    return loss, breaked_loss, final_state
+
+
+def beep():
+    duration = 1  # seconds
+    freq = 440  # Hz
+    os.system('play -nq -t alsa synth {} sine {}'.format(duration, freq))
+
